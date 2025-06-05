9624951496cf6dcf3c30a7595fd4497bfdfba097
Stanislav <sbalabushevich@lynxview.es>
2025-06-05 14:01:21 +0200

    Setup GitHub Actions different jar file name test

--- src/main/kotlin/com/slavant/testspringboot/controllers/StatusController.kt
+++ src/main/kotlin/com/slavant/testspringboot/controllers/StatusController.kt
@@ -10,6 +10,9 @@ class StatusController {
 
     @GetMapping
     fun getStatus(): String {
-        return "Everything cool! very well"
+        return "In this video, you'll learn how you can deploy a JAR-based backend (Spring Boot, Ktor, etc.) to a self-hosted VPS instance from Hetzner for just 3\$/month. \n" +
+                "I'll walk you through creating the VPS instance, connecting to it via SSH, securing it via a custom user account, setting up and running a systemd service," +
+                " setting up Nginx as a reverse-proxy with our " +
+                "own SSL certificate and custom domain and lastly how we can automate this deployment via a GitHub Actions pipeline when pushing to the master branch."
     }
 }
