004272b02c602d836cde22b494502843d8de3984
Stanislav <sbalabushevich@lynxview.es>
2025-06-05 12:59:23 +0200

    Setup GitHub Actions

--- src/main/kotlin/com/slavant/testspringboot/security/SecurityConfig.kt
+++ src/main/kotlin/com/slavant/testspringboot/security/SecurityConfig.kt
@@ -25,6 +25,8 @@ class SecurityConfig(
                 auth
                     .requestMatchers("/")
                     .permitAll()
+                    .requestMatchers("/")
+                    .permitAll()
                     .requestMatchers("/auth/**")
                     .permitAll()
                     .dispatcherTypeMatchers(
